b1530e623c73189566aed646ab90666c71ce8b38
yoav-klein <57894892+yoav-klein@users.noreply.github.com>
2023-02-01 16:34:11 +0200

    Update deployment.groovy

--- src/com/yoav/jenkins/deployment.groovy
+++ src/com/yoav/jenkins/deployment.groovy
@@ -1,4 +1,6 @@
 
+def branchList = ["a", "b", "c"]
+
 def deploymentClone(repoName, branch) {
     println "This the deploymentClone"
     println "Cloning $repoName, $branch"
@@ -6,5 +8,6 @@ def deploymentClone(repoName, branch) {
 
 def flow() {
     println "This is the flow function"
+    println "This is the branch list: $branchList"
     this.deploymentClone("sws-service", "main")
 }
