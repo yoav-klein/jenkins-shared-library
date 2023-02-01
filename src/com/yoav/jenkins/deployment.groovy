15a678e29d8006abc441fda1b74de79efb1d8c34
Yoav Klein <yoav.klein@cyberark.com>
2023-02-01 13:13:14 +0200

    first commit

---
+++ src/com/yoav/jenkins/deployment.groovy
@@ -0,0 +1,10 @@
+
+def deploymentClone(repoName, branch) {
+    println "This the deploymentClone"
+    println "Cloning $repoName, $branch"
+}
+
+def flow() {
+    println "This is the flow function"
+
+}
